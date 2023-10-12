c31a76bf3a04587be4f140b6aa17382a7f199731
DavitMatosyan <DavitMatosyan@DESKTOP-ASEK1MM>
2023-10-12 14:29:55 +0400

    Add project files.

---
+++ PandatechCrypto/AesHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography;
+
+namespace PandatechCrypto
+{
+    public static class AesHelper
+    {
+
+        private static readonly string Key = Environment.GetEnvironmentVariable("AES_KEY")!;
+
+        public static byte[] Encrypt(string plainText)
+        {
+
+            using var aesAlg = Aes.Create();
+            aesAlg.KeySize = 256;
+            aesAlg.Padding = PaddingMode.PKCS7;
+            aesAlg.Key = Convert.FromBase64String(Key);
+            aesAlg.GenerateIV();
+            
+            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using var msEncrypt = new MemoryStream();
+            using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            using var swEncrypt = new StreamWriter(csEncrypt);
+            swEncrypt.Write(plainText);
+            swEncrypt.Flush();
+            csEncrypt.FlushFinalBlock();
+
+            var encryptedPasswordByte = msEncrypt.ToArray();
+
+            byte[] result = aesAlg.IV.Concat(encryptedPasswordByte).ToArray();
+            return result;
+        }
+
+        public static string Decrypt(byte[] cipherText)
+        {
+            int splitIndex = 16;
+
+            byte[] iv = cipherText.Take(splitIndex).ToArray();
+            byte[] encrypted = cipherText.Skip(splitIndex).ToArray();
+
+            using var aesAlg = Aes.Create();
+            aesAlg.KeySize = 256;
+            aesAlg.Padding = PaddingMode.PKCS7;
+            aesAlg.Key = Convert.FromBase64String(Key);
+            aesAlg.IV = iv;
+
+            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using var msDecrypt = new MemoryStream(encrypted);
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            return srDecrypt.ReadToEnd();
+        }
+    }
+}
---
+++ PandatechCrypto/Argon2Helper.cs
@@ -0,0 +1,47 @@
+﻿using Konscious.Security.Cryptography;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PandatechCrypto
+{
+    public static class Argon2Helper
+    {
+        public static byte[] CreateSalt()
+        {
+            using var rng = RandomNumberGenerator.Create();
+            var buffer = new byte[16];
+            rng.GetBytes(buffer);
+            return buffer;
+        }
+
+        public static byte[] HashPassword(string password, byte[] salt, int memorySize = 128 * 1024)
+        {
+            using var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+            {
+                Salt = salt,
+                DegreeOfParallelism = 4,
+                Iterations = 4,
+                MemorySize = 256 * 1024
+
+            };
+
+            return argon2.GetBytes(32);
+        }
+
+        public static bool VerifyHash(string password, byte[] salt, byte[] hash)
+        {
+            var newHash = HashPassword(password, salt);
+            return ConstantTimeComparison(hash, newHash);
+        }
+
+        private static bool ConstantTimeComparison(byte[] a, byte[] b)
+        {
+            var diff = (uint)a.Length ^ (uint)b.Length;
+            for (var i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
+        }
+    }
+}
